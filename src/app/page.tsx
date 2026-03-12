f884825aa9abcde9b099b7a23274bbaf94af2e8e
Justin Mathew <jjmathew18@gmail.com>
2026-03-12 13:08:53 +0530

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div className="text-4xl font-bold text-green-500"> hello world!</div>;
+  return <Button variant="destructive">click me</Button>;
 }
